8e32ae8a7dea8770e4c71a58895af717d317dd38
shivamsen2098 <shivamsen2098@gmail.com>
2025-10-11 16:55:44 +0530

    upar diye code se token ban gaya aur print bhi karwa diya hai thunder par lekin ab hame ise client end me kese save karayenge vo janenges

--- backend/controller/admin.controller.js
+++ backend/controller/admin.controller.js
@@ -1,17 +1,23 @@
 const adminModel = require('../model/admin.model')
 const { noContentResponse, createdResponse, serverErrorResponse, errorResponse, successResponse, deletedResponse, updatedResponse } = require('../utility/response')
+var jwt = require('jsonwebtoken');
 
 const admin = {
 
     async login(req, res) {
         try {
-            console.log("Received:", req.body);
+            // console.log(req.body);
+            const { email, password } = req.body;
+            const admin = await adminModel.findOne({ email: email });
+            if (!admin) return errorResponse(res, "Admin not found")
+            if (password !== admin.password) return errorResponse(res, "Password not match")
 
-            const { email } = req.body;
-
-            if (!email) {
-                return errorResponse(res, "Email is required");
-            }
+                //add jwt function jo token ko create karega
+           const token =  jwt.sign({
+                id : admin._id,
+                email: admin.email
+            }, process.env.TOKEN_SECRET_KEY, { expiresIn: 7 * 24 * 60 * 60 * 1000 });
+            return successResponse(res, token,  "Admin login")
 
         } catch (error) {
             return serverErrorResponse(res, error.message);
